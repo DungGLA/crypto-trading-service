7f24a40719913d740053871aade2bb2c19e59947
dunggla <adung.hver23@gmail.com>
2026-06-19 23:51:29 +0700

    initial commit

---
+++ src/main/java/com/assignment/crypto_trading_service/CryptoTradingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.crypto_trading_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoTradingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoTradingServiceApplication.class, args);
+	}
+
+}
